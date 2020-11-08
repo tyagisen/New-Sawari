0dd1bbd237c56b9d11b46b3fe40bc31de41dc951
tyagisen <tyagisen@gmail.com>
2020-11-08 11:47:43 +0545

    All Done

--- sawariapp/models.py
+++ sawariapp/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 from .validators import validate_number
-from django.core import validators
-import requests
-
 
 
 class Driver(models.Model):
@@ -10,15 +7,5 @@ class Driver(models.Model):
     phone = models.CharField(max_length=10, unique=True, validators=[validate_number])
     vehicle_type = models.CharField(max_length=100)
 
-    def run(self, *args, **kwargs):
-        r = requests.post(
-            "http://aakashsms.com/admin/public/sms/v3/send/",
-            data={
-                'auth_token': '1271c391761e586555c3c0339d7403a22c6cf55e7fa0da30fc40aa0a506224e7',
-                'to': '9800991847',
-                'text': f"Name:{self.name} \n Phone: {self.phone} \n vehicle Type: {self.vehicle_type}"
-            })
-        status_code = r.status_code
-        response = r.text
-        response_json = r.json()
-emp1 = Driver()
+    def __str__(self):
+        return self.name
--- sawariapp/views.py
+++ sawariapp/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import *
+import requests
 
 
 def sign(request):
@@ -15,13 +16,12 @@ def sign(request):
         message4 = request.POST['vehicle_type']
         message = "Name: " + message1 + "\n" + "Phone: " + message3 + "\n" + "Vehicle Type: " + message4
         if fm.is_valid():
-            fm.save()
-            emp1.run()  # calling run function from models.py
-            # nm = fm.cleaned_data['name']
-            # ph = fm.cleaned_data['phone']
-            # vt = fm.cleaned_data['vehicle_type']
-            # driverform = Driver(name=nm, phone=ph, vehicle_type=vt)
-            # driverform.save()
+            nm = fm.cleaned_data['name']
+            ph = fm.cleaned_data['phone']
+            vt = fm.cleaned_data['vehicle_type']
+            driverform = Driver(name=nm, phone=ph, vehicle_type=vt)
+            driverform.save()
+
             send_mail(
                 'Driver Form',
                 message,
@@ -32,6 +32,21 @@ def sign(request):
             messages.add_message(request, messages.INFO, 'You Are Registerd')
             success = True
             fm = DriverForm()
+
+            def run():
+                r = requests.post(
+                    "http://aakashsms.com/admin/public/sms/v3/send/",
+                    data={
+                        'auth_token': 'e4f3525c9cf770776a4c67c97b77a3f9bdb4396fad47868c9a4f570efd255bcf',
+                        'to': '9800991847',
+                        'text': f'{message}'
+                    })
+                status_code = r.status_code
+                response = r.text
+                response_json = r.json()
+
+            run()  # calling run function from models.py
+
     else:
         fm = DriverForm()
     return render(request, 'index.html', {'form': fm, 'success': success})
